c8840e44eb163d36575abe5dd3f4ea519d605cfd
jonathanvargas2016 <jonathan.1996mds@gmail.com>
2019-01-19 12:12:32 -0500

    Validar
    implementacion de la clase validar

---
+++ Validar.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoSoftware
+{
+    class Validar
+    {
+        public  void SoloLetras(KeyPressEventArgs v)
+        {
+            if (Char.IsLetter(v.KeyChar))
+            {
+                v.Handled = false;
+            }
+            else if (Char.IsSeparator(v.KeyChar))
+            {
+                v.Handled = false;
+            }
+            else if (Char.IsControl(v.KeyChar))
+            {
+                v.Handled = false;
+            }
+            else
+            {
+                v.Handled = true;
+               // MessageBox.Show("Solo Letras");
+            }
+        }
+
+        public  void SoloNumeros(KeyPressEventArgs v)
+        {
+            if (Char.IsDigit(v.KeyChar))
+            {
+                v.Handled = false;
+            }
+            else if (Char.IsSeparator(v.KeyChar))
+            {
+                v.Handled = false;
+            }
+            else if (Char.IsControl(v.KeyChar))
+            {
+                v.Handled = false;
+            }
+            else
+            {
+                v.Handled = true;
+               // MessageBox.Show("Solo Numeros");
+            }
+        }
+
+        public  void NumerosDecimales(KeyPressEventArgs v)
+        {
+            if (Char.IsDigit(v.KeyChar))
+            {
+                v.Handled = false;
+            }
+            else if (Char.IsSeparator(v.KeyChar))
+            {
+                v.Handled = false;
+            }
+            else if (Char.IsControl(v.KeyChar))
+            {
+                v.Handled = false;
+            }
+            else if (v.KeyChar.ToString().Equals("."))
+            {
+                v.Handled = false;
+            }
+            else
+            {
+                v.Handled = true;
+               // MessageBox.Show("Solo numeros o numeros con punto decimal");
+            }
+        }
+
+
+
+
+    }
+}
