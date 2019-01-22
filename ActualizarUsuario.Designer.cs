f2bf21fe06ff0bd44e27641a6982c07c5b50808d
Henry Aguilar <henrysagq@gmail.com>
2019-01-22 04:55:04 -0500

    agregados modulos Jona
    Agregue modulos Jona al mio

---
+++ ActualizarUsuario.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace ProyectoSoftware
+{
+    partial class ActualizarUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.busUsuario = new System.Windows.Forms.TextBox();
+            this.updTel = new System.Windows.Forms.TextBox();
+            this.updTipo = new System.Windows.Forms.TextBox();
+            this.updPN = new System.Windows.Forms.TextBox();
+            this.updSN = new System.Windows.Forms.TextBox();
+            this.updPA = new System.Windows.Forms.TextBox();
+            this.updSA = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.updCedu = new System.Windows.Forms.TextBox();
+            this.actualizar = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cédula";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(282, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tipo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nombres";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(282, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Apellidos";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(77, 23);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(79, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Buscar Usuario";
+            // 
+            // busUsuario
+            // 
+            this.busUsuario.Location = new System.Drawing.Point(162, 20);
+            this.busUsuario.Name = "busUsuario";
+            this.busUsuario.Size = new System.Drawing.Size(179, 20);
+            this.busUsuario.TabIndex = 9;
+            this.busUsuario.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.busUsuario_KeyPress);
+            // 
+            // updTel
+            // 
+            this.updTel.Location = new System.Drawing.Point(27, 244);
+            this.updTel.Name = "updTel";
+            this.updTel.Size = new System.Drawing.Size(100, 20);
+            this.updTel.TabIndex = 10;
+            this.updTel.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // updTipo
+            // 
+            this.updTipo.Location = new System.Drawing.Point(285, 102);
+            this.updTipo.Name = "updTipo";
+            this.updTipo.Size = new System.Drawing.Size(211, 20);
+            this.updTipo.TabIndex = 11;
+            this.updTipo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // updPN
+            // 
+            this.updPN.Location = new System.Drawing.Point(27, 171);
+            this.updPN.Name = "updPN";
+            this.updPN.Size = new System.Drawing.Size(100, 20);
+            this.updPN.TabIndex = 12;
+            this.updPN.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // updSN
+            // 
+            this.updSN.Location = new System.Drawing.Point(133, 171);
+            this.updSN.Name = "updSN";
+            this.updSN.Size = new System.Drawing.Size(100, 20);
+            this.updSN.TabIndex = 13;
+            this.updSN.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox5_KeyPress);
+            // 
+            // updPA
+            // 
+            this.updPA.Location = new System.Drawing.Point(285, 171);
+            this.updPA.Name = "updPA";
+            this.updPA.Size = new System.Drawing.Size(100, 20);
+            this.updPA.TabIndex = 14;
+            this.updPA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox6_KeyPress);
+            // 
+            // updSA
+            // 
+            this.updSA.Location = new System.Drawing.Point(396, 171);
+            this.updSA.Name = "updSA";
+            this.updSA.Size = new System.Drawing.Size(100, 20);
+            this.updSA.TabIndex = 15;
+            this.updSA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox7_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(358, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 216);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Teléfono";
+            // 
+            // updCedu
+            // 
+            this.updCedu.Location = new System.Drawing.Point(27, 102);
+            this.updCedu.Name = "updCedu";
+            this.updCedu.Size = new System.Drawing.Size(206, 20);
+            this.updCedu.TabIndex = 18;
+            this.updCedu.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // actualizar
+            // 
+            this.actualizar.Location = new System.Drawing.Point(158, 340);
+            this.actualizar.Name = "actualizar";
+            this.actualizar.Size = new System.Drawing.Size(75, 23);
+            this.actualizar.TabIndex = 19;
+            this.actualizar.Text = "Actualizar";
+            this.actualizar.UseVisualStyleBackColor = true;
+            this.actualizar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(285, 340);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Atrás";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // ActualizarUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 375);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.actualizar);
+            this.Controls.Add(this.updCedu);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.updSA);
+            this.Controls.Add(this.updPA);
+            this.Controls.Add(this.updSN);
+            this.Controls.Add(this.updPN);
+            this.Controls.Add(this.updTipo);
+            this.Controls.Add(this.updTel);
+            this.Controls.Add(this.busUsuario);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ActualizarUsuario";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.ActualizarUsuario_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox busUsuario;
+        private System.Windows.Forms.TextBox updTel;
+        private System.Windows.Forms.TextBox updTipo;
+        private System.Windows.Forms.TextBox updPN;
+        private System.Windows.Forms.TextBox updSN;
+        private System.Windows.Forms.TextBox updPA;
+        private System.Windows.Forms.TextBox updSA;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox updCedu;
+        private System.Windows.Forms.Button actualizar;
+        private System.Windows.Forms.Button button3;
+    }
+}
